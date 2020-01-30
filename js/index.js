a9a653eb8414d85a1f114f31b64e0ad7ef6963f2
youssefwork <youssef.samih97@gmail.com>
2020-01-30 23:17:15 +0100

    begin with tabs inside accord

--- js/index.js
+++ js/index.js
@@ -22,6 +22,32 @@ const WidthChange = mq => {
         } else {
             const panel = acc[i].nextElementSibling;
             panel.style.display = "none"
+            acc[i].addEventListener("click", function() {
+                const panel = this.nextElementSibling;
+                for (let j = 0; j < acc.length; j++) {
+                    acc[j].childNodes[3].style.display = 'none';
+                    acc[j].style.background = 'none';
+                    acc[j].style.height = '300%';
+                    acc[j].style.maxHeight = '410px';
+                    acc[j].childNodes[1].childNodes[1].style.marginTop = "150%"
+                    const panel = acc[j].nextElementSibling;
+                    panel.style.display = "none"
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                        panel.style.width = "unset";
+                    }
+                }
+                if (panel.style.maxHeight) {
+                    panel.style.width = "unset";
+                    panel.style.display = "none"
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.display = "block"
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                    panel.style.position = "absolute";
+                    panel.style.width = "40%";
+                }
+            })
         }
     }
 }
